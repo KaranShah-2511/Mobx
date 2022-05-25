3687b1afdb658b75c605f9cb6436a5378c12b084
KaranShah-2511 <karansshah25@gmail.com>
2022-05-25 19:36:59 +0530

    delete function

--- src/component/Page2.js
+++ src/component/Page2.js
@@ -2,38 +2,59 @@ import React from "react";
 import { useNameStore } from "../global/NameContext";
 import { useObserver } from "mobx-react";
 import { useState } from "react";
+import { confirm } from "react-confirm-box";
 
 function Page2() {
+  const del = async (id, name) => {
+    console.log("id", id);
+    const result = await confirm("Are you sure?", id);
+    console.log(result);
+    console.log("name", name);
+    if (result) {
+      nameStore.removeName(id);
+      setdelmessage( name + " deleted");
+      //   return;
+    }
+    // console.log("You click No!");
+  };
+
   const nameStore = useNameStore();
   const [newName, setNewName] = useState();
   const [status, setStatus] = useState();
   const [error, setError] = useState();
+  const [delmessage, setdelmessage] = useState();
 
   const submit = (id) => {
     if (!newName) {
       setStatus("disabled");
       setError("Please enter a name");
-
     } else {
-      console.log("new Name", newName, "Id", id);
+      //   console.log("new Name", newName, "Id", id);
       setError("");
 
       nameStore.addName(newName, id);
-
-      setNewName(" ");
     }
+    setNewName("");
+    console.log("new Name", newName, "Id", id);
   };
 
   return useObserver(() => (
     <div>
       <h1>Page 2</h1>
+      <p>{delmessage}</p>
       {nameStore.names.map((a) => (
         <div style={{ display: "flex", padding: "20px" }}>
           <p style={{ padding: "10px" }}>{a.name}</p>
           <p style={{ padding: "10px" }}>Id: {a.id}</p>
-          <input onChange={(e) => setNewName(e.target.value)} type="text" />
+          <input
+            name={a.id}
+            onChange={(e) => setNewName(e.target.value)}
+            type="text"
+          />
           <p>{error}</p>
-          <button onClick={() => nameStore.removeName(a.id)}> Delete</button>
+          <button onClick={() => del(a.id, a.name)}> Delete</button>
+
+          {/* <button onClick={() => nameStore.removeName(a.id)}> Delete</button> */}
           {/* <button onClick={submit(a.id)}>submit</button> */}
           {/* <button onClick={(e) => nameStore.updateName(a.id, newName)}>
             submit
